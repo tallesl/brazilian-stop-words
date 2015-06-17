a62b607b07681c6f6780088bd327023f06429be9
Talles L <talleslasmar@gmail.com>
2015-06-17 13:34:30 -0300

    renamed filter to filterWords so it doesn't conflict with Array's filter

--- test/brazilian-stop-words.js
+++ test/brazilian-stop-words.js
@@ -9,55 +9,55 @@ it('should be an array of strings', function () {
 })
 
 it('should remove stop words from string', function () {
-  assert.equal(stopWords.filter('coisas que a gente se esquece de dizer'), 'coisas gente esquece dizer')
-  assert.equal(stopWords.filter('frases que o vento vem as vezes me lembrar'), 'frases vento vem vezes lembrar')
-  assert.equal(stopWords.filter('coisas que ficaram muito tempo por dizer'), 'coisas ficaram tempo dizer')
-  assert.equal(stopWords.filter('na canção do vento não se cansam de voar'), 'canção vento não cansam voar')
-  assert.equal(stopWords.filter('você pega o trem azul o sol na cabeça'), 'você pega trem azul sol cabeça')
-  assert.equal(stopWords.filter('o sol pega o trem azul você na cabeça'), 'sol pega trem azul você cabeça')
-  assert.equal(stopWords.filter('um sol na cabeça'), 'sol cabeça')
+  assert.equal(stopWords.filterWords('coisas que a gente se esquece de dizer'), 'coisas gente esquece dizer')
+  assert.equal(stopWords.filterWords('frases que o vento vem as vezes me lembrar'), 'frases vento vem vezes lembrar')
+  assert.equal(stopWords.filterWords('coisas que ficaram muito tempo por dizer'), 'coisas ficaram tempo dizer')
+  assert.equal(stopWords.filterWords('na canção do vento não se cansam de voar'), 'canção vento não cansam voar')
+  assert.equal(stopWords.filterWords('você pega o trem azul o sol na cabeça'), 'você pega trem azul sol cabeça')
+  assert.equal(stopWords.filterWords('o sol pega o trem azul você na cabeça'), 'sol pega trem azul você cabeça')
+  assert.equal(stopWords.filterWords('um sol na cabeça'), 'sol cabeça')
 })
 
 it('should remove stop words from array', function () {
   assert.deepEqual(
-    stopWords.filter([ 'coisas', 'que', 'a', 'gente', 'se', 'esquece', 'de', 'dizer' ]),
+    stopWords.filterWords([ 'coisas', 'que', 'a', 'gente', 'se', 'esquece', 'de', 'dizer' ]),
     [ 'coisas', 'gente', 'esquece', 'dizer' ]
   )
 
   assert.deepEqual(
-    stopWords.filter([ 'frases', 'que', 'o', 'vento', 'vem', 'as', 'vezes', 'me', 'lembrar' ]),
+    stopWords.filterWords([ 'frases', 'que', 'o', 'vento', 'vem', 'as', 'vezes', 'me', 'lembrar' ]),
     [ 'frases', 'vento', 'vem', 'vezes', 'lembrar' ]
   )
 
   assert.deepEqual(
-    stopWords.filter([ 'coisas', 'que', 'ficaram', 'muito', 'tempo', 'por', 'dizer' ]),
+    stopWords.filterWords([ 'coisas', 'que', 'ficaram', 'muito', 'tempo', 'por', 'dizer' ]),
     [ 'coisas', 'ficaram', 'tempo', 'dizer' ]
   )
 
   assert.deepEqual(
-    stopWords.filter([ 'na', 'canção', 'do', 'vento', 'não', 'se', 'cansam', 'de', 'voar' ]),
+    stopWords.filterWords([ 'na', 'canção', 'do', 'vento', 'não', 'se', 'cansam', 'de', 'voar' ]),
     [ 'canção', 'vento', 'não', 'cansam', 'voar' ]
   )
 
   assert.deepEqual(
-    stopWords.filter([ 'você', 'pega', 'o', 'trem', 'azul', 'o', 'sol', 'na', 'cabeça' ]),
+    stopWords.filterWords([ 'você', 'pega', 'o', 'trem', 'azul', 'o', 'sol', 'na', 'cabeça' ]),
     [ 'você', 'pega', 'trem', 'azul', 'sol', 'cabeça' ]
   )
 
   assert.deepEqual(
-    stopWords.filter([ 'o', 'sol', 'pega', 'o', 'trem', 'azul', 'você', 'na', 'cabeça' ]),
+    stopWords.filterWords([ 'o', 'sol', 'pega', 'o', 'trem', 'azul', 'você', 'na', 'cabeça' ]),
     [ 'sol', 'pega', 'trem', 'azul', 'você', 'cabeça' ]
   )
 
   assert.deepEqual(
-    stopWords.filter([ 'um', 'sol', 'na', 'cabeça' ]),
+    stopWords.filterWords([ 'um', 'sol', 'na', 'cabeça' ]),
     [ 'sol' , 'cabeça' ]
   )
 })
 
 it('should throw', function () {
-  assert.throws(function() { stopWords.filter(null) }, Error)
-  assert.throws(function() { stopWords.filter(undefined) }, Error)
-  assert.throws(function() { stopWords.filter(42) }, Error)
+  assert.throws(function() { stopWords.filterWords(null) }, Error)
+  assert.throws(function() { stopWords.filterWords(undefined) }, Error)
+  assert.throws(function() { stopWords.filterWords(42) }, Error)
 })
 
