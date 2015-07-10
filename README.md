09c7e743c166ae1a45adf6e07adf0e5bf3963657
Talles L <talleslasmar@gmail.com>
2015-07-10 13:27:03 -0300

    keeping things fresh

--- README.md
+++ README.md
@@ -1,21 +1,41 @@
 # Brazilian stop words
 
-[![build](https://travis-ci.org/tallesl/brazilian-stop-words.png)](https://travis-ci.org/tallesl/brazilian-stop-words)
-[![coverage](https://coveralls.io/repos/tallesl/brazilian-stop-words/badge.png?branch=master)](https://coveralls.io/r/tallesl/brazilian-stop-words?branch=master)
-[![dependencies](https://david-dm.org/tallesl/brazilian-stop-words.png)](https://david-dm.org/tallesl/brazilian-stop-words)
-[![devDependencies](https://david-dm.org/tallesl/brazilian-stop-words/dev-status.png)](https://david-dm.org/tallesl/brazilian-stop-words#info=devDependencies)
-[![npm module](https://badge.fury.io/js/brazilian-stop-words.png)](http://badge.fury.io/js/brazilian-stop-words)
+[![][build-img]][build]
+[![][coverage-img]][coverage]
+[![][dependencies-img]][dependencies]
+[![][devdependencies-img]][devdependencies]
+[![][module-img]][module]
 
-[![npm](https://nodei.co/npm/brazilian-stop-words.png?mini=true)](https://nodei.co/npm/brazilian-stop-words/)
+[![][npm-img]][npm]
 
 A set of Brazilian stop words.
 
-The module itself is an array of words but there's also a `filter` that receives a text and filters out stop words from it.
+The module itself is an array of words but there's also a `filterWords` function that receives a text and filters out
+stop words from it.
+
+[build]:     https://travis-ci.org/tallesl/brazilian-stop-words)
+[build-img]: https://travis-ci.org/tallesl/brazilian-stop-words.png
+
+[coverage]:     https://coveralls.io/r/tallesl/brazilian-stop-words?branch=master
+[coverage-img]: https://coveralls.io/repos/tallesl/brazilian-stop-words/badge.png?branch=master
+
+[dependencies]:     https://david-dm.org/tallesl/brazilian-stop-words
+[dependencies-img]: https://david-dm.org/tallesl/brazilian-stop-words.png
+
+[devdependencies]:     https://david-dm.org/tallesl/brazilian-stop-words#info=devDependencies
+[devdependencies-img]: https://david-dm.org/tallesl/brazilian-stop-words/dev-status.png
+
+[module]:     http://badge.fury.io/js/brazilian-stop-words
+[module-img]: https://badge.fury.io/js/brazilian-stop-words.png
+
+[npm]:     https://nodei.co/npm/brazilian-stop-words
+[npm-img]: https://nodei.co/npm/brazilian-stop-words.png?mini=true
 
 ## Usage
-```javascript
+
+```js
 $ npm install brazilian-stop-words
-brazilian-stop-words@1.0.0 node_modules/brazilian-stop-words
+brazilian-stop-words@2.0.1 node_modules/brazilian-stop-words
 $ node
 > var brStopWords = require('brazilian-stop-words')
 undefined
--- test/brazilian-stop-words.js
+++ test/brazilian-stop-words.js
@@ -1,63 +1,94 @@
-var assert    = require('assert')
-  , stopWords = require('../lib/brazilian-stop-words')
+var assert            = require('assert')
+  , brStopWords       = require('../lib/brazilian-stop-words')
+  , NotArrayNorString = require('../lib/errors/not-array-nor-string')
+  , NonStringInArray  = require('../lib/errors/non-string-in-array')
 
 it('should be an array of strings', function () {
-  assert(Array.isArray(stopWords))
-  stopWords.forEach(function (word) {
-    assert.equal(typeof word, 'string')
-  })
+
+  assert(Array.isArray(brStopWords))
+  brStopWords.forEach(function (word) { assert.equal(typeof word, 'string') })
+
 })
 
-it('should remove stop words from string', function () {
-  assert.equal(stopWords.filterWords('coisas que a gente se esquece de dizer'), 'coisas gente esquece dizer')
-  assert.equal(stopWords.filterWords('frases que o vento vem as vezes me lembrar'), 'frases vento vem vezes lembrar')
-  assert.equal(stopWords.filterWords('coisas que ficaram muito tempo por dizer'), 'coisas ficaram tempo dizer')
-  assert.equal(stopWords.filterWords('na canção do vento não se cansam de voar'), 'canção vento não cansam voar')
-  assert.equal(stopWords.filterWords('você pega o trem azul o sol na cabeça'), 'você pega trem azul sol cabeça')
-  assert.equal(stopWords.filterWords('o sol pega o trem azul você na cabeça'), 'sol pega trem azul você cabeça')
-  assert.equal(stopWords.filterWords('um sol na cabeça'), 'sol cabeça')
+it('should filter stop words from a single string', function () {
+
+  assert.equal(
+      brStopWords.filterWords('coisas que a gente se esquece de dizer')
+    ,                         'coisas gente esquece dizer')
+
+  assert.equal(
+      brStopWords.filterWords('frases que o vento vem as vezes me lembrar')
+    ,                         'frases vento vem vezes lembrar')
+
+  assert.equal(
+      brStopWords.filterWords('coisas que ficaram muito tempo por dizer')
+    ,                         'coisas ficaram tempo dizer')
+
+  assert.equal(
+      brStopWords.filterWords('na canção do vento não se cansam de voar')
+    ,                         'canção vento não cansam voar')
+
+  assert.equal(
+      brStopWords.filterWords('você pega o trem azul o sol na cabeça')
+    ,                         'você pega trem azul sol cabeça')
+
+  assert.equal(
+      brStopWords.filterWords('o sol pega o trem azul você na cabeça')
+    ,                         'sol pega trem azul você cabeça')
+
+  assert.equal(
+      brStopWords.filterWords('um sol na cabeça')
+    ,                         'sol cabeça')
+
 })
 
-it('should remove stop words from array', function () {
-  assert.deepEqual(
-    stopWords.filterWords([ 'coisas', 'que', 'a', 'gente', 'se', 'esquece', 'de', 'dizer' ]),
-    [ 'coisas', 'gente', 'esquece', 'dizer' ]
-  )
+it('should filter stop words from an array of strings', function () {
 
   assert.deepEqual(
-    stopWords.filterWords([ 'frases', 'que', 'o', 'vento', 'vem', 'as', 'vezes', 'me', 'lembrar' ]),
-    [ 'frases', 'vento', 'vem', 'vezes', 'lembrar' ]
-  )
+      brStopWords.filterWords([ 'coisas', 'que', 'a', 'gente', 'se', 'esquece', 'de', 'dizer' ])
+    ,                         [ 'coisas', 'gente', 'esquece', 'dizer' ])
 
   assert.deepEqual(
-    stopWords.filterWords([ 'coisas', 'que', 'ficaram', 'muito', 'tempo', 'por', 'dizer' ]),
-    [ 'coisas', 'ficaram', 'tempo', 'dizer' ]
-  )
+      brStopWords.filterWords([ 'frases', 'que', 'o', 'vento', 'vem', 'as', 'vezes', 'me', 'lembrar' ])
+    ,                         [ 'frases', 'vento', 'vem', 'vezes', 'lembrar' ])
 
   assert.deepEqual(
-    stopWords.filterWords([ 'na', 'canção', 'do', 'vento', 'não', 'se', 'cansam', 'de', 'voar' ]),
-    [ 'canção', 'vento', 'não', 'cansam', 'voar' ]
-  )
+      brStopWords.filterWords([ 'coisas', 'que', 'ficaram', 'muito', 'tempo', 'por', 'dizer' ])
+    ,                         [ 'coisas', 'ficaram', 'tempo', 'dizer' ])
 
   assert.deepEqual(
-    stopWords.filterWords([ 'você', 'pega', 'o', 'trem', 'azul', 'o', 'sol', 'na', 'cabeça' ]),
-    [ 'você', 'pega', 'trem', 'azul', 'sol', 'cabeça' ]
-  )
+      brStopWords.filterWords([ 'na', 'canção', 'do', 'vento', 'não', 'se', 'cansam', 'de', 'voar' ])
+    ,                         [ 'canção', 'vento', 'não', 'cansam', 'voar' ])
 
   assert.deepEqual(
-    stopWords.filterWords([ 'o', 'sol', 'pega', 'o', 'trem', 'azul', 'você', 'na', 'cabeça' ]),
-    [ 'sol', 'pega', 'trem', 'azul', 'você', 'cabeça' ]
-  )
+      brStopWords.filterWords([ 'você', 'pega', 'o', 'trem', 'azul', 'o', 'sol', 'na', 'cabeça' ])
+    ,                         [ 'você', 'pega', 'trem', 'azul', 'sol', 'cabeça' ])
 
   assert.deepEqual(
-    stopWords.filterWords([ 'um', 'sol', 'na', 'cabeça' ]),
-    [ 'sol' , 'cabeça' ]
-  )
+      brStopWords.filterWords([ 'o', 'sol', 'pega', 'o', 'trem', 'azul', 'você', 'na', 'cabeça' ])
+    ,                         [ 'sol', 'pega', 'trem', 'azul', 'você', 'cabeça' ])
+
+  assert.deepEqual(
+      brStopWords.filterWords([ 'um', 'sol', 'na', 'cabeça' ])
+    ,                         [ 'sol' , 'cabeça' ])
+
 })
 
-it('should throw', function () {
-  assert.throws(function() { stopWords.filterWords(null) }, Error)
-  assert.throws(function() { stopWords.filterWords(undefined) }, Error)
-  assert.throws(function() { stopWords.filterWords(42) }, Error)
+it('should throw when given not a string nor array', function () {
+
+  assert.throws(function () { brStopWords.filterWords()      }, NotArrayNorString)
+  assert.throws(function () { brStopWords.filterWords(null)  }, NotArrayNorString)
+  assert.throws(function () { brStopWords.filterWords(123)   }, NotArrayNorString)
+  assert.throws(function () { brStopWords.filterWords(/lol/) }, NotArrayNorString)
+
 })
+
+it('should throw when there\'s a non string in array', function () {
+
+  assert.throws(function () { brStopWords.filterWords([ '1', 2, '3' ])}, NonStringInArray)
+
+})
+
+
+
 
